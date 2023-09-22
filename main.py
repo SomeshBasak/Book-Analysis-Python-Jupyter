dfecc316dd9315666fcd514ee5c3b3125f712705
som <someshbasak2016@gmail.com>
2023-09-23 01:40:06 +0530

    Final Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Done this project on jupyter lab
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+english_stopwords = stopwords.words("english")
+
+print(english_stopwords)
